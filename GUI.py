d91a79f36aa5fe18847e724cc6c19f996d009c5a
andres620 <andres.1701625573@ucaldas.edu.co>
2019-07-25 10:46:14 -0500

    hicimos la ventana y pintamos un fondo

--- GUI.py
+++ GUI.py
@@ -17,11 +17,12 @@ class GUI:
         self.sHEIGHT = 600    #screen height
         self.font=None
         self.screen=None
+        self.cityA=pygame.image.load("images/GPS.png")
         
         
     def window(self):
         pygame.init()
-        pygame.display.set_caption("Árbol NArio")
+        pygame.display.set_caption("Soy el mapa")
         self.screen=pygame.display.set_mode((self.sWIDTH, self.sHEIGHT),RESIZABLE)
         self.screen.fill((254,245,231))
         self.font = pygame.font.SysFont("Arial", 12)
@@ -31,6 +32,7 @@ class GUI:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit() 
+            self.paint(self.graph)
             pygame.display.update()
             
             
@@ -38,8 +40,5 @@ class GUI:
     
     def paint(self,graph):
         if not graph is None:
-            for h in graph.places:
-                h.assingColor()
-                pygame.display.flip()
-            for h in graph.places:
-                self.paint(h)
+            self.screen.blit(self.cityA,(100,50))
+            pygame.display.flip()
